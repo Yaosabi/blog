54d6c8c7a26cb1528f6ac9bd9ad3291b98042a33
Yaosabi <lorenzoimurillo@outlook.com>
2022-04-01 22:07:48 -0700

    finished project

---
+++ controllers/articleController.js
@@ -0,0 +1,81 @@
+const {Article, Comment, Reply} = require('../models');
+
+module.exports.renderAddForm = function(req,res){
+    const article = {
+        title: '',
+        intro: '',
+        image_url: '',
+        body: ''
+    };
+    res.render('articles/add', {articles});
+}
+
+module.exports.addArticle = async function(req,res){
+    const article = await Article.create({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+        author_id: 1,
+        published_on: new Date()
+    });
+    res.redirect('/')
+}
+
+module.exports.displayArticle = async function(req,res){
+    const article = await Article.findByPk(req.params.articleId, {
+        include: [
+            'author',
+            {
+                model: Comment,
+                as: 'comments',
+                required:false,
+                include: [{
+                    model:Reply,
+                    as:'replies',
+                    required: false
+                }]
+            }
+        ],
+        order: [
+            ['comments','commented_on','desc']
+        ]
+    });
+    res.render('articles/view', {article});
+}
+
+module.exports.displayAll = async function(req,res){
+    const articles = await Article.findAll({
+        include: ['author']
+    });
+    res.render('articles/viewAll')
+}
+
+module.exports.renderEditForm = async function(req,res){
+    const article = await Article.findByPk(req.params.articleId);
+    res.render('articles/edit', {articles});
+}
+
+module.exports.updateArticle = async function(req,res){
+    await Article.update({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+    }, {
+        where:{
+            id: req.params.articleId
+        }
+    });
+    res.redirect(`/article/${req.params.articleId}`)
+}
+
+module.exports.deleteArticle = async function(req,res){
+    await Article.destroy({
+        where:{
+            id: req.params.articleId
+        }
+    });
+    res.redirect('/')
+}
+
